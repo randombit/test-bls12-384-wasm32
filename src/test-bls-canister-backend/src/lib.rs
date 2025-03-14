61becd792bcf2acecd6c758223ada5ae154cfc5a
Jack Lloyd <jack.lloyd@dfinity.org>
2025-03-14 09:30:45 -0400

    Use 0.10.1, import ic_bls12_381

--- src/test-bls-canister-backend/src/lib.rs
+++ src/test-bls-canister-backend/src/lib.rs
@@ -1,3 +1,8 @@
+use ic_bls12_381::{
+    hash_to_curve::{ExpandMsgXmd, HashToCurve},
+    multi_miller_loop, G1Affine, G1Projective, G2Affine, G2Prepared, G2Projective, Gt, Scalar,
+};
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
